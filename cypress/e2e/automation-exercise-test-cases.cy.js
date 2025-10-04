eb2a41cfbbfc83172c2cb33bd1e1f12d717567be
Jenifer Silva <jssilva423@gmail.com>
2025-10-04 11:33:55 -0300

    rename files and improve upload step on contact us test

--- cypress/e2e/automation-exercise-test-cases.cy.js
+++ cypress/e2e/automation-exercise-test-cases.cy.js
@@ -1,11 +1,12 @@
 /// <reference types="cypress" />
 
 import userData from "../fixtures/user-data.json";
-import homePage from "../page_objects/home-page";
+import headerPage from "../page_objects/header-page";
 import loginPage from "../page_objects/login-page";
 import signupPage from "../page_objects/signup-page";
 import accountCreatedPage from "../page_objects/account-created-page";
 import accountDeletedPage from "../page_objects/account-deleted-page";
+import contactUsPage from "../page_objects/contact-us-page";
 
 describe("Automation Exercise", () => {
   beforeEach(() => {
@@ -14,7 +15,7 @@ describe("Automation Exercise", () => {
 
   it("Test Case 1: Register User", () => {
     let email = `qa-tester-${new Date().getTime()}@test.com`;
-    homePage.elements.loginBtn().click();
+    headerPage.elements.loginBtn().click();
     loginPage.startSignup(userData.name, email);
 
     signupPage.fillAccountInformation(userData.password, {
@@ -27,22 +28,22 @@ describe("Automation Exercise", () => {
       .accountCreatedHeader()
       .should("have.text", "Account Created!");
     accountCreatedPage.elements.continueBtn().click();
-    homePage.elements.loggedInAsText(userData.name).should("be.visible");
+    headerPage.elements.loggedInAsText(userData.name).should("be.visible");
 
-    homePage.elements.deleteAccountBtn().click();
+    headerPage.elements.deleteAccountBtn().click();
     accountDeletedPage.elements
       .accountDeletedHeader()
       .should("have.text", "Account Deleted!");
   });
 
   it("Test Case 2: Login User with correct email and password", () => {
-    homePage.elements.loginBtn().click();
+    headerPage.elements.loginBtn().click();
     loginPage.login(userData.email, userData.password);
-    homePage.elements.loggedInAsText(userData.name).should("be.visible");
+    headerPage.elements.loggedInAsText(userData.name).should("be.visible");
   });
 
   it("Test Case 3: Login User with incorrect email and password", () => {
-    homePage.elements.loginBtn().click();
+    headerPage.elements.loginBtn().click();
     loginPage.login("test@test.com", "123");
     loginPage.elements
       .loginErrorText()
@@ -50,18 +51,37 @@ describe("Automation Exercise", () => {
   });
 
   it("Test Case 4: Logout User", () => {
-    homePage.elements.loginBtn().click();
+    headerPage.elements.loginBtn().click();
     loginPage.login(userData.email, userData.password);
-    homePage.elements.loggedInAsText(userData.name).should("be.visible");
-    homePage.elements.logoutBtn().click();
+    headerPage.elements.loggedInAsText(userData.name).should("be.visible");
+    headerPage.elements.logoutBtn().click();
     cy.url().should("include", "/login");
   });
 
   it("Test Case 5: Register User with existing email", () => {
-    homePage.elements.loginBtn().click();
+    headerPage.elements.loginBtn().click();
     loginPage.startSignup(userData.name, userData.email);
     loginPage.elements
       .signupErrorText()
       .should("have.text", "Email Address already exist!");
+  });
+
+  it.only("Test Case 6: Contact Us Form", () => {
+    headerPage.elements.contactUsBtn().click();
+    cy.url().should("include", "/contact_us");
+    cy.contains('Get In Touch');
+
+    contactUsPage.elements.nameInput().type(userData.name);
+    contactUsPage.elements.emailInput().type(userData.email);
+    contactUsPage.elements.subjectInput().type(userData.subject);
+    contactUsPage.elements.messageInput().type(userData.message);
+    cy.fixture('example.json').as('file')
+    contactUsPage.elements.uploadFile().selectFile('@file')
+    contactUsPage.elements.submitBtn().click();
+    
+    cy.get(".status").should("have.text", "Success! Your details have been submitted successfully.");
+
+    headerPage.elements.homeBtn().click();
+    cy.url().should("include", "/");
   });
 });
--- cypress/page_objects/header-page.js
+++ cypress/page_objects/header-page.js
@@ -1,11 +1,12 @@
-class HomePage {
+class HeaderPage {
   elements = {
     loginBtn: () => cy.get('a[href="/login"]'),
     loggedInAsText: (username) => cy.contains(`Logged in as ${username}`),
     deleteAccountBtn: () => cy.get('a[href="/delete_account"]'),
     logoutBtn: () => cy.get("a[href='/logout']"),
-    contactUsBtn: () => cy.get("a[href='/contact_us']")
+    contactUsBtn: () => cy.get("a[href='/contact_us']"),
+    homeBtn: () => cy.get(".fa-home")
   };
 }
 
-export default new HomePage();
+export default new HeaderPage();
