e2235b7c7d29d3e25896d630a18ed88ce44dce19
Jenifer Silva <jssilva423@gmail.com>
2025-10-06 19:58:53 -0300

    Fix test

--- cypress/e2e/automation-exercise-test-cases.cy.js
+++ cypress/e2e/automation-exercise-test-cases.cy.js
@@ -327,7 +327,7 @@ describe("Automation Exercise - Modularizarion", () => {
     products.addProductToCartFromList(0);
     products.closeCartModal();
     products.addProductToCartFromList(1);
-    header.goToCart();
+    products.goToCart();
     checkout.checkBagItems(2);
     checkout.goToCheckout();
 
